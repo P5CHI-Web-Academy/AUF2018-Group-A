696733544956496158a9dd6e62700eca62ca27a3
Adrian Gherman <agherman@pentalog.com>
2018-12-03 15:43:04 +0200

    was verified algorithm dijkstra with the JSON example

--- stand-alone-project/parsing_filters/FilterExample.py
+++ stand-alone-project/parsing_filters/FilterExample.py
@@ -1,10 +1,23 @@
+import sys
+sys.path.insert(0, './dijkstra')
+
 from Path import *
-from Edge import Edge
+from Edge import *
+from dijkstra import *
 
 dataArray = get_data_from_file('file.json')
 
 paths = [Path(data) for data in dataArray]
 
-edges = Edge.edges_from_paths_by_filtering(paths, CarriageType.auto, CostFunction.time)
+edges = Edge.edges_from_paths_by_filtering(paths, CarriageType.train, CostFunction.time)
 
-print(len(edges))
+for edge in edges:
+    print("\n", edge.start, edge.end, edge.cost)
+
+covered_edges = dijkstra(edges, "#1", "#4")
+
+print("covered_edges:")
+for edge in covered_edges:
+    print(edge.start, edge.end, edge.cost)
+
+print ("total_cost =", total_cost(covered_edges))

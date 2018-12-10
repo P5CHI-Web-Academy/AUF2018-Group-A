ae8e2ae6e1350e9ac5894ea0fffb92e9586f8195
tvadim <vtrifan@pentalog.com>
2018-12-10 14:27:56 +0200

    Added models: Edge, Vertice

---
+++ Hproj-mobile/MProj/src/Models/Edge.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+class Edge {
+    start
+    end
+    cost
+
+    constructor(edgeData) {
+
+        this.start = edgeData.start;
+        this.end = edgeData.end;
+        this.cost = edgeData.cost;
+
+        }
+    }
+export default class Edge;

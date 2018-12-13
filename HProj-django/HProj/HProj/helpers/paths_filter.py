298f2224b3e6d1caf3e9cf15e7b7fac66b9e23e6
vCiumac <vciumac@pentalog.com>
2018-12-13 12:02:15 +0200

    reworked filters workflow / dijkstra call

--- HProj-django/HProj/HProj/helpers/paths_filter.py
+++ HProj-django/HProj/HProj/helpers/paths_filter.py
@@ -3,8 +3,11 @@ from HProj.models.Path import Path, CarriageType
 from HProj.models.Carriage import CostFunction, Carriage
 from HProj.models.Edge import Edge
 
-def get_edges_from_url(url):
+def get_edges_from_url(url, carriage_type, cost_function):
     dataArray = get_data_from_url(url)
     paths = [Path(data) for data in dataArray]
-    edges = Edge.edges_from_paths_by_filtering(paths, CarriageType.auto, CostFunction.time)
+    carriage_type = carriage_type if carriage_type is not None else CarriageType.auto
+    cost_function = cost_function if cost_function is not None else CostFunction.price
+    edges = Edge.edges_from_paths_by_filtering(paths, carriage_type, cost_function)
+
     return edges
